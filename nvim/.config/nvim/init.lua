8b48a2191253915098b641653693596175f4bb0c
Liberis Patoucheas <libpatouch@gmail.com>
2024-10-19 22:34:14 +0300

    Change to lazy-vim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,43 +1,56 @@
--- Initialize packer.nvim
-require('impatient')
-require('packer').startup(function(use)
-    use 'lewis6991/impatient.nvim'
+-- Load Lazy.nvim automatically
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Lazy.nvim setup
+require("lazy").setup({
     -- Packer manages itself
-    use 'wbthomason/packer.nvim'
-    
-    -- ColorSchemes (lazy load on event like ColorScheme)
-    use {'aliqyan-21/darkvoid.nvim', event = "VimEnter"}
-    use {'EdenEast/nightfox.nvim', event = "VimEnter"}
+    { 'wbthomason/packer.nvim' },
+    { 'lewis6991/impatient.nvim' },
 
-    -- Git integration (lazy load on Git commands)
-    use {'tpope/vim-fugitive', cmd = { 'Git', 'G' }}
+    -- ColorSchemes
+    { 'aliqyan-21/darkvoid.nvim', event = "VimEnter" },
+    { 'EdenEast/nightfox.nvim', event = "VimEnter" },
 
-    -- Gitsigns (lazy load on buffer event)
-    use {
+    -- Git Integration
+    { 'tpope/vim-fugitive', cmd = { 'Git', 'G' } },
+
+    -- Gitsigns
+    {
         'lewis6991/gitsigns.nvim',
         event = "BufRead",
-        requires = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('gitsigns').setup({})
         end
-    }
+    },
 
-    -- LSP Config (lazy load on specific commands)
-    use {'neovim/nvim-lspconfig', event = "BufReadPre"}
-    use {'williamboman/mason.nvim', cmd = "Mason"}
-    use {'williamboman/mason-lspconfig.nvim', after = 'mason.nvim'}
+    -- LSP Config
+    { 'neovim/nvim-lspconfig', event = "BufReadPre" },
+    { 'williamboman/mason.nvim', cmd = "Mason" },
+    { 'williamboman/mason-lspconfig.nvim', dependencies = { 'mason.nvim' } },
 
     -- Autocompletion
-    use {'hrsh7th/nvim-cmp', event = "InsertEnter"}
-    use {'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp'}
-    use {'L3MON4D3/LuaSnip', event = "InsertEnter"}
-    use {'saadparwaiz1/cmp_luasnip', after = 'LuaSnip'}
+    { 'hrsh7th/nvim-cmp', event = "InsertEnter" },
+    { 'hrsh7th/cmp-nvim-lsp', dependencies = { 'nvim-cmp' } },
+    { 'L3MON4D3/LuaSnip', event = "InsertEnter" },
+    { 'saadparwaiz1/cmp_luasnip', dependencies = { 'LuaSnip' } },
 
-    -- Treesitter (lazy load on file type)
-    use {
+    -- Treesitter
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
         event = "BufRead",
+        build = ':TSUpdate',
         config = function()
             require('nvim-treesitter.configs').setup {
                 highlight = {
@@ -46,26 +59,27 @@ require('packer').startup(function(use)
                 }
             }
         end
-    }
+    },
 
-    -- Markdown preview (lazy load on specific command)
-    use { 'ellisonleao/glow.nvim', cmd = "Glow" }
+    -- Markdown Preview
+    { 'ellisonleao/glow.nvim', cmd = "Glow" },
 
-    -- Null-ls (lazy load for linting/formatting)
-    use {'jose-elias-alvarez/null-ls.nvim', event = "BufReadPre"}
+    -- Null-ls
+    { 'jose-elias-alvarez/null-ls.nvim', event = "BufReadPre" },
 
-    -- Neo-tree (lazy load when file explorer is opened)
-    use {
+    -- Neo-tree
+    {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
         cmd = "Neotree",
-        requires = {
+        dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim",
         }
-    }
-end)
+    },
+})
+
 -- Set ColorScheme
 vim.cmd [[colorscheme vim]]
 vim.g.mapleader = ' '
@@ -103,6 +117,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.incsearch = true
 vim.o.hlsearch = true
+
 -- Mason setup to manage LSP installations
 require("mason").setup()
 
@@ -147,6 +162,7 @@ lspconfig.rust_analyzer.setup({
         }
     },
 })
+
 -- Generic LSP settings
 local on_attach = function(_, bufnr)
     local opts = { noremap=true, silent=true }
@@ -192,7 +208,6 @@ cmp.setup({
     },
 })
 
-
 require('nvim-treesitter.configs').setup {
     ensure_installed = {
         "go", "python", "yaml", "json", "bash", "java", "terraform", "hcl", "kotlin", "helm", "rust"
@@ -202,7 +217,6 @@ require('nvim-treesitter.configs').setup {
         additional_vim_regex_highlighting = false,
     },
 }
-
 
 local null_ls = require('null-ls')
 
@@ -222,34 +236,34 @@ null_ls.setup({
 
         -- Shell scripting (bash)
         null_ls.builtins.formatting.shfmt,
-
     }
 })
+
 -- Neo-tree setup with NERDTree-like behavior
 require("neo-tree").setup({
-  close_if_last_window = true,  -- Close Neo-tree when it's the last window
-  enable_git_status = true,     -- Show git status for files
-  enable_diagnostics = true,    -- Show diagnostics in file explorer
+  close_if_last_window = true,
+  enable_git_status = true,
+  enable_diagnostics = true,
   filesystem = {
     filtered_items = {
-      hide_dotfiles = false,    -- Show dotfiles by default (NERDTree-like)
-      hide_gitignored = false,  -- Show Git ignored files
+      hide_dotfiles = false,
+      hide_gitignored = false,
     },
-    follow_current_file_enabled = true, -- Focus on current file when opening Neo-tree
-    use_libuv_file_watcher = true, -- Asynchronous file updates
+    follow_current_file_enabled = true,
+    use_libuv_file_watcher = true,
   },
   window = {
-    position = "left",          -- Tree appears on the left (NERDTree style)
-    width = 30,                 -- Set the width for the file tree
+    position = "left",
+    width = 30,
     mappings = {
-      ["<CR>"] = "open",        -- Open file or folder
-      ["l"] = "open",           -- Open file or expand folder (NERDTree style)
-      ["h"] = "close_node",     -- Close folder node (NERDTree style)
-      ["t"] = "open_tabnew",    -- Open file in a new tab
-      ["v"] = "open_vsplit",    -- Open file in a vertical split
-      ["s"] = "open_split",     -- Open file in a horizontal split
-      ["i"] = "toggle_hidden",  -- Toggle visibility of dotfiles (like NERDTree)
-      ["R"] = "refresh",        -- Refresh the tree
+      ["<CR>"] = "open",
+      ["l"] = "open",
+      ["h"] = "close_node",
+      ["t"] = "open_tabnew",
+      ["v"] = "open_vsplit",
+      ["s"] = "open_split",
+      ["i"] = "toggle_hidden",
+      ["R"] = "refresh",
     }
   },
   default_component_configs = {
@@ -259,10 +273,10 @@ require("neo-tree").setup({
       with_markers = true,
     },
     icon = {
-      folder_closed = "",  -- Folder closed icon
-      folder_open = "",    -- Folder open icon
-      folder_empty = "",   -- Empty folder icon
-      default = "",        -- Default file icon
+      folder_closed = "",
+      folder_open = "",
+      folder_empty = "",
+      default = "",
     },
     git_status = {
       symbols = {
@@ -279,61 +293,37 @@ require("neo-tree").setup({
     }
   },
 })
+
 -- Format the buffer
 vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+
 -- Keybindings for LSP diagnostics navigation
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap=true, silent=true })
 
-
-
 -- Git keybindings with leader key and 'g' as Git prefix
 local opts = { noremap = true, silent = true }
 
--- Git Status (Interactive)
+-- Git commands
 vim.api.nvim_set_keymap('n', '<leader>gs', ':G<CR>', opts)
-
--- Git Add All (git add -A)
 vim.api.nvim_set_keymap('n', '<leader>ga', ':Git add -A<CR>', opts)
-
--- Git Add Updated (git add -u)
 vim.api.nvim_set_keymap('n', '<leader>gu', ':Git add -u<CR>', opts)
-
--- Git Add Current File
 vim.api.nvim_set_keymap('n', '<leader>gA', ':Git add %<CR>', opts)
-
--- Git Commit (Opens editor for message)
 vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', opts)
-
--- Git Commit with Message Inline
 vim.api.nvim_set_keymap('n', '<leader>gC', ':Git commit -m ""<Left>', { noremap = true })
-
--- Git Push
 vim.api.nvim_set_keymap('n', '<leader>gp', ':Git push<CR>', opts)
-
--- Git Pull
 vim.api.nvim_set_keymap('n', '<leader>gl', ':Git pull<CR>', opts)
-
--- Git Fetch
 vim.api.nvim_set_keymap('n', '<leader>gf', ':Git fetch<CR>', opts)
-
--- Git Log
 vim.api.nvim_set_keymap('n', '<leader>gh', ':Git log<CR>', opts)
-
--- Git Diff
 vim.api.nvim_set_keymap('n', '<leader>gD', ':Git diff<CR>', opts)
-
--- Git Branch
 vim.api.nvim_set_keymap('n', '<leader>gb', ':Git branch<CR>', opts)
-
--- Git Checkout
 vim.api.nvim_set_keymap('n', '<leader>gco', ':Git checkout ', { noremap = true })
-
--- Select Files to Add/Remove (Interactive)
 vim.api.nvim_set_keymap('n', '<leader>gd', ':G<CR>', opts)
+
 -- File Explorer
 vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
 -- Markdown preview
 vim.api.nvim_set_keymap('n', '<leader>md', ':Glow<CR>', { noremap = true, silent = true })
+
